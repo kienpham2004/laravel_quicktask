e104108c0d11aab7f1f798e39575f15ef8aac973
kienpham2004 <phamkien20041998@gmail.com>
2020-11-12 09:35:03 +0700

    prepping route, controller, middleware and localization

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'UserController@index')->name('user.index');
+Route::get('order/index', 'OrderController@tindex')->name('order.index');
+Route::get('user/{lang}', 'HomeController@setLocale')->name('user.lang');
+
+Route::group(['middleware'=>'locale'], function () {
+    Route::group(['prefix' => 'order'], function () {
+        Route::get('index', 'OrderController@index')->name('order.index');
+        Route::get('create', 'OrderController@create')->name('order.create');
+        Route::post('store', 'OrderController@store')->name('order.store');
+        Route::get('edit/{id}', 'OrderController@edit')->name('order.edit');
+        Route::put('update/{id}', 'OrderController@update')->name('order.update');
+        Route::delete('destroy/{id}', 'OrderController@destroy')->name('order.destroy');
+        Route::get('back', 'OrderController@back')->name("order.back");
+    });
+    
+    Route::group(['prefix' => 'users'], function () {
+        Route::get('index', 'UserController@index')->name('user.index');
+        Route::get('create', 'UserController@create')->name('user.create');
+        Route::post('store', 'UserController@store')->name('user.store');
+        Route::get('edit/{id}', 'UserController@edit')->name('user.edit');
+        Route::put('update/{id}', 'UserController@update')->name('user.update');
+        Route::delete('destroy/{id}', 'UserController@destroy')->name('user.destroy');
+        Route::get('back', 'UserController@back')->name("user.back");
+    });
 });
